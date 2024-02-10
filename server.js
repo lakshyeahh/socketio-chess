57ab2aa3119d848eb1c781f3e57c56547b779abf
lakshyeahh <verma.lakshya071@gmail.com>
2024-02-10 12:09:54 +0530

    NEW

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-const http = require('http');
 const express = require('express');
-const socketIo = require('socket.io');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-// Socket.IO logic
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    // Listen for move events from clients
-    socket.on('move', (moveData) => {
-        console.log('Received move:', moveData);
+  socket.on('buttonPressed', (data) => {
+    console.log("someone moved");
+    // Emit the direction to all other connected clients except the sender
+    socket.broadcast.emit('buttonPressed', data);
+  });
 
-        // Broadcast the move to all other clients
-        socket.broadcast.emit('move', moveData);
-    });
-
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
 });
